f3cbfdc25a93df270f1bf9ad3671a5ab9d4219b5
Syamkrishna MG <syamkrishna.m@hogrefe.com>
2025-03-04 21:10:02 +0100

    Adding the sort code

---
+++ Practical Part/sort.js
@@ -0,0 +1,33 @@
+function sort(input) {
+    
+    if (!input) {
+        console.error("Enter valid input");
+        return input;
+    }
+
+    const temp = input.match(/\d+|[a-zA-Z]|\W/g) || [];
+    let num = [], lower = [], upper = [], special = [];
+
+    temp.forEach(n => {
+        if (/^\d+$/.test(n)) {
+            num.push(parseInt(n, 10));
+        } else if (/[a-z]/.test(n)) {
+            lower.push(n);
+        } else if (/[A-Z]/.test(n)) {
+            upper.push(n);
+        } else {
+            special.push(n);
+        }
+    });
+
+    num.sort((a, b) => a - b);
+    lower.sort();
+    upper.sort();
+    special.sort();
+    return [...num, ...lower, ...upper, ...special].join('');
+}
+
+
+//console.log(sort('A11a4'));
+//console.log(sort('abCA1da')); 
+//console.log(sort(''));
